57a3f919cf8bda00eef08b454e821253546e4e4e
Eliton <ti@tinturariacave.com.br>
2026-07-07 15:23:06 -0300

    style: apply terraform fmt to vpc.tf to fix ci lint

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = { Name = "${var.project_name}-${var.environment}-vpc" }
+  tags                 = { Name = "${var.project_name}-${var.environment}-vpc" }
 }
 
 resource "aws_subnet" "public" {
@@ -10,19 +10,19 @@ resource "aws_subnet" "public" {
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "${var.aws_region}a"
-  tags = { Name = "${var.project_name}-${var.environment}-public" }
+  tags                    = { Name = "${var.project_name}-${var.environment}-public" }
 }
 
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "${var.aws_region}a"
-  tags = { Name = "${var.project_name}-${var.environment}-private" }
+  tags              = { Name = "${var.project_name}-${var.environment}-private" }
 }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
-  tags = { Name = "${var.project_name}-${var.environment}-igw" }
+  tags   = { Name = "${var.project_name}-${var.environment}-igw" }
 }
 
 resource "aws_route_table" "public" {
